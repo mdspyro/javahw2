b04fd684a67a83425273ed5d7f548c94c1f01bcd
Spyro <124052906+mdspyro@users.noreply.github.com>
2023-10-08 17:07:58 +0400

    change

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main  {
     public static void main(String[] args) {
         // Цена билета
-        int x = 15855;
+        int price = 15855;
 
         // Количество рублей за одну милю
-        double d = 20.0;
+        double rub = 20.0;
 
         // Количество начисленных бонусов
-        int xx = (int) (x / d);
+        int bonus = (int) (price / rub);
 
-        System.out.println(" При стоимости билета в " + x + " рублей начислено " + xx + " бонуса.");
+        System.out.println(" При стоимости билета в " + price + " рублей начислено " + bonus + " бонуса.");
     }
 }
 
